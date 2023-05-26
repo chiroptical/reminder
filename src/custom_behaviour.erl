ee9b740d6185ed305245ce8acc48470df136874b
chiroptical <chiroptical@gmail.com>
2023-05-25 21:23:35 -0400

    On to state machines

--- src/custom_behaviour.erl
+++ src/custom_behaviour.erl
@@ -4,8 +4,10 @@
 -export([behaviour_info/1]).
 
 behaviour_info(callbacks) ->
-    [{init, 1},
-     {some_fun, 0},
-     {other, 3}
+    [
+        {init, 1},
+        {some_fun, 0},
+        {other, 3}
     ];
-behaviour_info(_) -> undefined.
+behaviour_info(_) ->
+    undefined.
--- src/kitty_gen_server.erl
+++ src/kitty_gen_server.erl
@@ -1,2 +1,89 @@
+%% Changing the example a bit
+%%
+%% This is an adoption center
+%%
+%% You can:
+%% - drop off a new cat
+%% - adopt a cat
+%% - return a cat
 -module(kitty_gen_server).
 -behaviour(gen_server).
+-export([
+    start_link/0,
+    init/1,
+    handle_cast/2,
+    handle_call/3,
+    handle_info/2,
+    terminate/2,
+    code_change/3
+]).
+
+-export([
+    adopt_cat/4,
+    drop_off/2,
+    start_sanctuary/1
+]).
+
+-type color() :: red | orange | yellow | green | blue | indigo | violet.
+
+-record(cat, {
+    name :: string(),
+    color = green :: color(),
+    description :: string()
+}).
+-type cat() :: #cat{}.
+
+-spec cat(string(), color(), string()) -> cat().
+cat(Name, Color, Description) ->
+    #cat{name = Name, color = Color, description = Description}.
+
+%% Arguments to gen_server:start_link
+%% 1. the callback module
+%% 2. parameter to init/1, i.e. an empty FIFO queue
+%% 3. debugging options
+-spec start_link() -> {atom(), pid()}.
+start_link() ->
+    gen_server:start_link(?MODULE, queue:new(), []).
+
+-spec adopt_cat(pid(), string(), color(), string()) -> any().
+adopt_cat(Pid, Name, Color, Description) ->
+    gen_server:call(Pid, {adopt, Name, Color, Description}).
+
+-spec drop_off(pid(), cat()) -> any().
+drop_off(Pid, Cat = #cat{}) ->
+    gen_server:cast(Pid, {drop_off, Cat}).
+
+-spec start_sanctuary(pid()) -> any().
+start_sanctuary(Pid) ->
+    gen_server:call(Pid, sanctuary).
+
+-spec init(queue:queue()) -> any().
+init(Queue) ->
+    {ok, Queue}.
+
+handle_call({adopt, Name, Color, Description}, _From, Cats) ->
+    case queue:out(Cats) of
+        %% No cats in the shelter, find the cat they want
+        {empty, _} ->
+            {reply, cat(Name, Color, Description), Cats};
+        %% Cats in the shelter, they adopt the one present in the
+        %% shelter longest.
+        {{value, Cat}, Next} ->
+            {reply, Cat, Next}
+    end;
+handle_call(sanctuary, _From, Cats) ->
+    {stop, normal, ok, Cats}.
+
+handle_cast({drop_off, Cat = #cat{}}, Cats) ->
+    {noreply, queue:in(Cat, Cats)}.
+
+handle_info(Msg, Cats) ->
+    io:format("Unexpected message: ~p~n", [Msg]),
+    {noreply, Cats}.
+
+terminate(normal, Cats) ->
+    [io:format("~p was set free!~n", [C#cat.name]) || C <- queue:to_list(Cats)],
+    ok.
+
+code_change(_OldVersion, State, _Extra) ->
+    {ok, State}.
