10cd04b581ed00ede67c73e5da2e50e1e09256e2
chiroptical <chiroptical@gmail.com>
2023-06-22 16:22:15 -0400

    Set up some TDD for make_change

--- src/vending_machine.erl
+++ src/vending_machine.erl
@@ -19,12 +19,16 @@
     off/3
 ]).
 
+%% For testing
 -export([
     add_coin/2,
-    optimize_coins/3
+    optimize_coins/3,
+    make_change/3
 ]).
 
 -type coin() :: nickel | dime | quarter.
+
+-define(COST_OF_SODA, 150).
 
 -record(state, {
     inserted_coins = maps:new() :: maps:iterator(coin(), pos_integer()),
@@ -66,22 +70,16 @@ on({call, From}, request_soda, S = #state{}) ->
         {reply, From, []}
     ]};
 on({call, From}, {insert_coin, Coin}, S = #state{inserted_coins = Coins}) ->
-    case coins_value(Coins) >= 150 of
-        true ->
-            {next_state, on, S, [
-                {reply, From, {no_insert, Coin}}
-            ]};
-        false ->
-            %% TODO: Ideally we minimize the overall value
-            {next_state, on,
-                S#state{
-                    inserted_coins =
-                        maps:update_with(Coin, fun(Count) -> Count + 1 end, 1, Coins)
-                },
-                [
-                    {reply, From, {inserted, Coin}}
-                ]}
-    end.
+    {ok, CoinsToReturn, Optimized} = optimize_coins(
+        ?COST_OF_SODA, add_coin(Coin, Coins), map:empty()
+    ),
+    {next_state, on,
+        S#state{
+            inserted_coins = Optimized
+        },
+        [
+            {reply, From, {insert, {coin, Coin}, {returned, CoinsToReturn}}}
+        ]}.
 
 off({call, From}, flip_switch, S = #state{}) ->
     {next_state, on, S, [
@@ -119,21 +117,30 @@ add_coin(Coin, Coins) ->
 recurse_optimize(Coin, Limit, Coins, Optimized) ->
     optimize_coins(Limit - coin_value(Coin), Coins, add_coin(Coin, Optimized)).
 
+%% Given a cost, coins in the machine, and bank, attempt to make change for the
+%% customer. If we are unable, return an error for now. We may ask the user to
+%% press the button to confirm we can move forward without providing change. We
+%% can also let the customer know when they interact with the machine without
+%% any nickels we can't make change.
+%% TODO: Finish this function, see tests in vending_machine_tests.erl
+make_change(_Cost, _Coins, _Bank) ->
+    ok.
+
 %% Given a limit, e.g. 150 cents, get as close to that as we possibly can
 %% without going over.
 optimize_coins(Limit, Coins, Optimized) when Limit =< 0 ->
-    {optimized, Coins, Optimized};
+    {ok, Coins, Optimized};
 optimize_coins(Limit, Coins, Optimized) when Limit =< 5 ->
     case select_smallest_possible_coin(Coins) of
         {Coin, NewCoins} ->
-            {optimized, NewCoins, add_coin(Coin, Optimized)};
+            {ok, NewCoins, add_coin(Coin, Optimized)};
         empty ->
-            {optimized, Coins, Optimized}
+            {ok, Coins, Optimized}
     end;
 optimize_coins(Limit, Coins, Optimized) ->
     case select_largest_possible_coin(Limit, Coins) of
         {Coin, NewCoins} -> recurse_optimize(Coin, Limit, NewCoins, Optimized);
-        empty -> {optimized, Coins, Optimized}
+        empty -> {ok, Coins, Optimized}
     end.
 
 subtract_coin(Coin, Coins) ->
--- test/vending_machine_tests.erl
+++ test/vending_machine_tests.erl
@@ -5,44 +5,44 @@ add_coin_test() ->
     ?assertEqual(vending_machine:add_coin(dime, maps:new()), #{dime => 1}).
 
 optimize_coins_nothing_to_optimize_should_work_test() ->
-    {optimized, Coins, Optimized} = vending_machine:optimize_coins(100, maps:new(), maps:new()),
+    {ok, Coins, Optimized} = vending_machine:optimize_coins(100, maps:new(), maps:new()),
     0 = maps:size(Coins),
     0 = maps:size(Optimized).
 
 optimize_coins_remove_quarter_should_work_test() ->
     Coins = #{quarter => 1},
-    {optimized, NewCoins, Optimized} = vending_machine:optimize_coins(25, Coins, maps:new()),
+    {ok, NewCoins, Optimized} = vending_machine:optimize_coins(25, Coins, maps:new()),
     0 = maps:size(NewCoins),
     ?assertEqual(Optimized, Coins).
 
 optimize_coins_remove_quarters_should_work_test() ->
     Coins = #{quarter => 4},
-    {optimized, NewCoins, Optimized} = vending_machine:optimize_coins(100, Coins, maps:new()),
+    {ok, NewCoins, Optimized} = vending_machine:optimize_coins(100, Coins, maps:new()),
     0 = maps:size(NewCoins),
     ?assertEqual(Optimized, Coins).
 
 optimize_coins_one_fifty_from_quarters_dimes_test() ->
     Coins = #{quarter => 4, dime => 5},
-    {optimized, NewCoins, Optimized} = vending_machine:optimize_coins(150, Coins, maps:new()),
+    {ok, NewCoins, Optimized} = vending_machine:optimize_coins(150, Coins, maps:new()),
     0 = maps:size(NewCoins),
     ?assertEqual(Optimized, Coins).
 
 optimize_coins_select_smallest_test() ->
     Coins = #{nickel => 1, dime => 1},
-    {optimized, NewCoins, Optimized} = vending_machine:optimize_coins(5, Coins, maps:new()),
+    {ok, NewCoins, Optimized} = vending_machine:optimize_coins(5, Coins, maps:new()),
     ?assertEqual(#{dime => 1}, NewCoins),
     ?assertEqual(#{nickel => 1}, Optimized).
 
 %% Goes over 150 because we have no nickels
 optimize_coins_one_fifty_from_quarters_dimes_over_test() ->
     Coins = #{quarter => 3, dime => 8},
-    {optimized, NewCoins, Optimized} = vending_machine:optimize_coins(150, Coins, maps:new()),
+    {ok, NewCoins, Optimized} = vending_machine:optimize_coins(150, Coins, maps:new()),
     0 = maps:size(NewCoins),
     ?assertEqual(Coins, Optimized).
 
 optimize_coins_select_nickel_over_dime_test() ->
     Coins = #{quarter => 1, dime => 1, nickel => 1},
-    {optimized, NewCoins, Optimized} = vending_machine:optimize_coins(30, Coins, maps:new()),
+    {ok, NewCoins, Optimized} = vending_machine:optimize_coins(30, Coins, maps:new()),
     ?assertEqual(#{dime => 1}, NewCoins),
     ?assertEqual(#{quarter => 1, nickel => 1}, Optimized).
 
@@ -51,6 +51,25 @@ optimize_coins_select_nickel_over_dime_test() ->
 %% and quarters.
 optimize_coins_select_dime_over_nickel_test() ->
     Coins = #{quarter => 1, dime => 1, nickel => 1},
-    {optimized, NewCoins, Optimized} = vending_machine:optimize_coins(31, Coins, maps:new()),
+    {ok, NewCoins, Optimized} = vending_machine:optimize_coins(31, Coins, maps:new()),
     ?assertEqual(#{nickel => 1}, NewCoins),
     ?assertEqual(#{quarter => 1, dime => 1}, Optimized).
+
+make_change_none_necessary_test() ->
+    Bank = #{},
+    Coins = #{quarter => 1},
+    {ok, Change, NewBank} = vending_machine:make_change(25, Coins, Bank),
+    ?assertEqual(#{quarter => 1}, NewBank),
+    ?assertEqual(#{}, Change).
+
+make_change_simple_test() ->
+    Bank = #{nickel => 1},
+    Coins = #{quarter => 1},
+    {ok, Change, NewBank} = vending_machine:make_change(20, Coins, Bank),
+    ?assertEqual(#{nickel => 1}, Change),
+    0 = maps:size(NewBank).
+
+make_change_unable_to_make_test() ->
+    Bank = #{},
+    Coins = #{quarter => 1},
+    unable_to_make_change = vending_machine:make_change(20, Coins, Bank).
